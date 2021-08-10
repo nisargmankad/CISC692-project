d3178703a0478a59dd3874d879724042557564a7
nisargmankad <nisarg.mankad@gmail.com>
2021-08-09 22:17:35 -0400

    First push

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const health = require('@cloudnative/health-connect');
 const express = require('express');
 const mongoose = require('mongoose');
+let healthcheck = new health.HealthChecker();
 
 const app = express();
-
+app.use('/live', health.LivenessEndpoint(healthcheck))
+app.use('/ready', health.ReadinessEndpoint(healthcheck))
+app.use('/health', health.HealthEndpoint(healthcheck))
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: false }));
